727636bc66df5708c14aa5e618785840e12d0df7
Elienai Alves de Souza <DA1J@192.168.0.122>
2025-07-11 11:15:47 -0300

    Cria a classe Endereco (embeddable) e incorpora em Restaurante

--- src/main/java/com/elienai/springfood/domain/model/Restaurante.java
+++ src/main/java/com/elienai/springfood/domain/model/Restaurante.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.Column;
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -13,6 +14,8 @@ import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -36,6 +39,10 @@ public class Restaurante {
 	@ManyToOne
 	@JoinColumn(name = "cozinha_id", nullable = false)
 	private Cozinha cozinha;
+	
+	@JsonIgnore
+	@Embedded
+	private Endereco endereco;
 	
 	@ManyToMany
 	@JoinTable(name = "restaurante_forma_pagamento",
