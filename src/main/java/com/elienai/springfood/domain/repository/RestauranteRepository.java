f739725097569a69752e6eaf09f87ff3358c7f9c
Elienai Alves de Souza <DA1J@192.168.0.122>
2025-07-11 12:33:06 -0300

    Resolve o Problema do N+1 com fetch join na JPQL na lista de Restaurante

--- src/main/java/com/elienai/springfood/domain/repository/RestauranteRepository.java
+++ src/main/java/com/elienai/springfood/domain/repository/RestauranteRepository.java
@@ -1,11 +1,17 @@
 package com.elienai.springfood.domain.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.elienai.springfood.domain.model.Restaurante;
 
 @Repository
 public interface RestauranteRepository extends JpaRepository<Restaurante, Long> {
+	
+	@Query("from Restaurante r join fetch r.cozinha left join fetch r.formasPagamento")
+	List<Restaurante> findAll();
 	
 }
