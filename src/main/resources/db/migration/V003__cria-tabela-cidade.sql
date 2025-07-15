26b21314acf4eaadcf2c5c4d4f83b67fd4504c43
Elienai Alves de Souza <DA1J@192.168.0.122>
2025-07-15 16:17:06 -0300

    Cria scripts de migração do flyway "db/migration"

---
+++ src/main/resources/db/migration/V003__cria-tabela-cidade.sql
@@ -0,0 +1,10 @@
+create table cidade (
+	id bigint not null auto_increment, 
+	nome varchar(80) not null, 
+	estado_id bigint not null, 
+	
+	primary key (id)
+) engine=InnoDB default charset=utf8;
+
+alter table cidade add constraint fk_cidade_estado
+foreign key (estado_id) references estado (id);
