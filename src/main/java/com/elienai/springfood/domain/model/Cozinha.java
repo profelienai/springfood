805a4bf585d352853cad4038476aaaf293f1441b
Elienai Alves de Souza <DA1J@192.168.0.122>
2025-08-22 18:31:35 -0300

    Inclui anotações de validação nas entidades principais. Cidade, Cozinha, Estado e Restaurante

--- src/main/java/com/elienai/springfood/domain/model/Cozinha.java
+++ src/main/java/com/elienai/springfood/domain/model/Cozinha.java
@@ -9,6 +9,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
@@ -20,11 +22,13 @@ import lombok.EqualsAndHashCode;
 @Entity
 public class Cozinha {
 
+	@NotNull
 	@EqualsAndHashCode.Include
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
+	@NotBlank
 	@Column(nullable = false)
 	private String nome;
 	
